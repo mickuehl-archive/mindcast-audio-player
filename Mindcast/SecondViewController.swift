705eacc4e461e5af5aaec1c5465f73a9d63cb962
Michael Kuehl <mickuehl@ratchet.cc>
2015-03-09 18:11:34 +0100

    Initial Commit

---
+++ Mindcast/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mindcast
+//
+//  Created by Michael Kuehl on 09.03.15.
+//  Copyright (c) 2015 ratchet.cc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
